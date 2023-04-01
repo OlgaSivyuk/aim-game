0865bcfd8277cac2c55f65a93cdc61af70a5032e
o.sivyuk <olchik0308@yandex.ru>
2023-04-01 16:01:11 +0400

    add colores

--- app.js
+++ app.js
@@ -56,21 +56,51 @@ function finishGame(){
   board.innerHTML = `<h1>Cчет: <span class='primary'>${score}</span></h1>`
 };
 
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = `linear-gradient(90deg, ${color[0]} 0%, ${color[1]} 47%, ${color[2]} 100%)`;
+};
+
+function getRandomColor() {
+    const groupColors = [
+        ['#F06651', '#FF6799', '#F183E3'],
+        ['#37EF8C', '#A4E35B', '#E3D247'],
+        ['#FF01C0', '#A147B9', '#6E3B96'],
+        ['#16d9e3', '#30c7ec', '#46aef7'],
+        ['#B69531', '#A53040', '#350F58'],
+        ['#53206F', '#9163A1', '#CFA9D7'],
+        ['#5BD20B', '#00ACE7', '#4F4BDB'],
+
+    ]
+    const groupIndex = Math.floor(Math.random() * groupColors.length);
+    const group = groupColors[groupIndex];
+    // const colorIndex = Math.floor(Math.random() * group.length);
+    // const color = group[colorIndex];
+    console.log(group);
+    return group;
+  }
+
 function createRandomCircle() {
   const circle = document.createElement('div');
   const size = getRundomNumber(10, 60);
   const { width, height } = board.getBoundingClientRect();
   const x = getRundomNumber(0, width - size);
   const y = getRundomNumber(0, height - size);
+//   const color = getRandomColor();
   circle.classList.add('circle');
   circle.style.width = `${size}px`;
   circle.style.height = `${size}px`;
   circle.style.top = `${y}px`;
   circle.style.left = `${x}px`;
-
-  board.append(circle)
+//   circle.style.background = `linear-gradient(90deg, ${color[0]} 0%, ${color[1]} 47%, ${color[2]} 100%)`;
+  setColor(circle)
+  board.append(circle);
 };
 
 function getRundomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min)
-}
+};
+
+
+
+
